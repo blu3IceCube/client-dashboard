4bbd3ca1c8c5263a0f43ab17ee4b93d4e6a1547c
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-06-30 10:01:58 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Placeholder Title</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae voluptate, harum nesciunt qui asperiores culpa distinctio tempora blanditiis consequuntur facere, ullam, ut esse nisi neque provident quod? Quis, perspiciatis dolore?</p>
+    </div>
+  )
+}
