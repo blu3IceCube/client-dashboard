8bf5794fa499aa25d9fdeba2a6c4dfaf9044573b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-09-13 12:24:10 +0530

    adding dashboard

--- scenes/Row2.tsx
+++ scenes/Row2.tsx
@@ -3,35 +3,61 @@ import { useGetKpisQuery, useGetProductsQuery } from "@/app/api/state/api";
 import BoxHeader from "@/components/BoxHeader";
 import {
   CartesianGrid,
+  Cell,
   Line,
   LineChart,
+  Pie,
+  PieChart,
   ResponsiveContainer,
+  Scatter,
+  ScatterChart,
   Tooltip,
   XAxis,
   YAxis,
+  ZAxis,
 } from "recharts";
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { useMemo } from "react";
+import FlexContainer from "@/components/FlexContainer";
 
-type Props = {};
+const pieData = [
+  { name: "Group A", value: 600 },
+  { name: "Group B", value: 400 },
+];
 
-const Row2 = (props: Props) => {
+const Row2 = () => {
   const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[300]];
   const { data: operationalData } = useGetKpisQuery();
   const { data: productsData } = useGetProductsQuery();
 
   const operationalExpenses = useMemo(() => {
     return (
       operationalData &&
-      operationalData[0].monthlyData.map(({ month, operationalExpenses, nonOperationalExpenses }) => {
-        return {
-          name: month.substring(0, 3),
-          "Operational Expenses": operationalExpenses,
-          "Non Operational Expenses": nonOperationalExpenses
-        };
-      })
+      operationalData[0].monthlyData.map(
+        ({ month, operationalExpenses, nonOperationalExpenses }) => {
+          return {
+            name: month.substring(0, 3),
+            "Operational Expenses": operationalExpenses,
+            "Non Operational Expenses": nonOperationalExpenses,
+          };
+        }
+      )
     );
   }, [operationalData]);
+
+  const productExpenseData = useMemo(() => {
+    return (
+      productsData &&
+      productsData.map(({ _id, price, expense}) => {
+        return {
+          id: _id,
+          price: price,
+          expense: expense
+        }
+      })
+    )
+  }, [productsData])
 
   return (
     <>
@@ -87,8 +113,104 @@ const Row2 = (props: Props) => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
+      <DashboardBox gridArea="e">
+        <BoxHeader title="Campaigns and Targets" sideText="+4%" />
+        <FlexContainer mt="0.25rem" gap="1.5rem" pr="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
+          >
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+              style={{ position: "absolute" }}
+            >
+              {pieData &&
+                pieData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                ))}
+            </Pie>
+          </PieChart>
+          <Box
+            ml="-0.7rem"
+            flexBasis="40%"
+            textAlign="center"
+            justifyContent="center"
+            display="flex"
+            flexDirection="column"
+          >
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+              83
+            </Typography>
+            <Typography variant="h6">
+              Finance goals of the campaign that is desired
+            </Typography>
+          </Box>
+          <Box
+            flexBasis="40%"
+            justifyContent="center"
+            display="flex"
+            flexDirection="column"
+          >
+            <Typography variant="h5">Losses in Revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">
+              Profit Margins
+            </Typography>
+            <Typography variant="h6">
+              Margins are up 30% from last month.
+            </Typography>
+          </Box>
+        </FlexContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="f">
+        <BoxHeader title="Product Prices vs Expenses" sideText="+4%" />
+        <ResponsiveContainer width="100%" height="100%">
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 25,
+              bottom: 40,
+              left: -10,
+            }}
+            style={{ position: "absolute" }}
+          >
+            <CartesianGrid stroke={palette.grey[800]} />
+            <XAxis
+              type="number"
+              dataKey="price"
+              name="price"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              tickFormatter={(v) => `$${v}`}
+            />
+            <YAxis
+              type="number"
+              dataKey="expense"
+              name="expense"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              tickFormatter={(v) => `$${v}`}
+            />
+            <ZAxis type="number" range={[20]} />
+            <Tooltip formatter={(v) => `$${v}`} />
+            <Scatter name="Product Expense Ratio" data={productExpenseData} fill={palette.tertiary[500]} />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </DashboardBox>
     </>
   );
 };
